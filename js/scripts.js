835bf27329910d3b7ef72ec8d743c602e46ff57a
Gustavo <gustaraujosouza@gmail.com>
2021-10-27 14:18:13 -0300

    variaveis

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//variaveis
+
+var inteiro = 18 
+var flutuante = 1.8
+var texto = 'textinnho'
+
+var papelHigienico = {
+    cor: 'branco',
+    tamanho: 15,
+    preco: .15
+}
+
+var mattiz = [17, 
+    'texto', 
+    1.9, {
+    nome: 'Gustavo'
+}]
+
+
+
+console.log(papelHigienico)
+
+
+
+
+papelHigienico = 50
+
+console.log(papelHigienico)
