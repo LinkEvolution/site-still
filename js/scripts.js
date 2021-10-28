74ca7ef7ae21b483135f04b0ac72db2a8563fe9a
Gustavo <gustaraujosouza@gmail.com>
2021-10-28 13:53:42 -0300

    jscriptdiavinteoito

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //variaveis
-
-var inteiro = 18 
+/*
+var inteiro = '6'
 var flutuante = 1.8
 var texto = 'textinnho'
 
@@ -16,13 +16,53 @@ var mattiz = [17,
     nome: 'Gustavo'
 }]
 
+if (inteiro >= 10) {
+    console.log('Maior que 10')
+} else if (inteiro === '6') { 
+console.log('Nao e texto')
+} else {
+    console.log('Menor que 10')
+}
+/*
+const comparacao = prompt('Mensagem, digitte aqio')
 
+switch (comparacao) {
+    case '2':
+        console.log('Ele e um o numero 6')
+        break;
+    case '5':
+        console.log('Ele e o 5')
+        break;
+    case '6':
+        console.log('Ele e um o numero 6')
+        break;
+    case '3':
+        console.log('Ele e um o numero 3')
+        break;
 
-console.log(papelHigienico)
+    default:
+        console.log('Não encontrei')
+        break;
+}
+*/
 
+/*
+const comparacao = prompt('Mensagem, digite aqui:')
 
+if (comparacao === 10) {
+    console.log('é 10')
+} else if (comparacao < 10) {
+    console.log('e um numero 10')
+} else if (comparacao > 10) {
+    console.log('Isso e um 10')
+} else {
+    console.log('Nao achei nao')
+}
 
+*/
+/*
+const comparacao = prompt('Mensagem, digite aqui:')
 
-papelHigienico = 50
+const ternaria = (comparacao === '10') ? console.log('E igual a 10') : console.log('Nao e igual a 10'); 
 
-console.log(papelHigienico)
+*/
